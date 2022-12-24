20c173e372bab98b24f8aaf1cc0fb1820962e869
Qq24gg <113111975+Qq24gg@users.noreply.github.com>
2022-12-24 14:19:31 +0300

    Add files via upload

--- lab4.py
+++ lab4.py
@@ -1,10 +1,30 @@
-a = int(input())
-b = int(input())
-c = int(input())
+numbers = [54, 32, 23, 75, 5, 23, 76, 30, 95, 10]
+ 
+a = 0
+ 
+for i in numbers:
+    a += i
+ 
+print(a)
 
-if (a == b or a == c or b == c) and not (a == b and b == c):
-    print('2')
-elif a == b and b == c:
-    print('1')
-else:
-    print('3')
+
+
+a = 0
+numbers = [0, 8, 2, 8, 0, 3, 0, 2, 69, 0, 46, 90, 0]
+for i in numbers:
+    if i == 0:
+        a += 1
+print(a)
+
+
+
+n = int(input())
+for i in range(1, n+1):
+    print(" "*(i-1)+""+ str(i))
+
+
+
+n = int(input()) + 1
+for i in range(1, n + 1):
+    print(' ' * (n - i), end='')
+    print(*range(1, i), *range(1, i - 1)[::-1], sep='')
