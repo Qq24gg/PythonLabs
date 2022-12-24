d938c466243657b1b41abd85afd41d4e6c600788
Qq24gg <113111975+Qq24gg@users.noreply.github.com>
2022-12-24 14:44:45 +0300

    Add files via upload

---
+++ Лабараторная №2/lab23.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if (b and c) < a:
+    print(a, 'наибольшее')
+elif (a and c) < b:
+    print(b, 'наибольшее')
+else:
+    print(c, 'наибольшее')
